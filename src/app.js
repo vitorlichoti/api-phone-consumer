7e116573c60a53165276fe8047803d3abff98219
Vitor <vitorlichoti@gmail.com>
2024-03-04 23:17:24 -0400

    adiciona min.css nas docs

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const productRouter = require('./routes/product.routes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('../docs/swaggerOptions');
 
-const CSS_URL = "https://cdn.bootcdn.net/ajax/libs/swagger-ui/5.6.2/swagger-ui-bundle.js";
+const CSS_URL = "https://cdn.bootcdn.net/ajax/libs/swagger-ui/5.6.2/swagger-ui.css";
 
 const app = express();
 
