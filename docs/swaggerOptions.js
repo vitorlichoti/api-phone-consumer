00ff1825ff4bbd0d09058334a95ff98203fb8448
Vitor <vitorlichoti@gmail.com>
2024-03-04 23:03:55 -0400

    adiciona min.css nas docs

--- docs/swaggerOptions.js
+++ docs/swaggerOptions.js
@@ -11,7 +11,7 @@ const options = {
     servers: [
       {
         url: 'https://api-phone-consumer.vercel.app',
-        url: 'http://localhost:3000',
+        description: "My API Documentation",
       },
     ],
   },
