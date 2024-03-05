6857a6b2c1f3c8597be5e4344c7c7e4ef113d754
Vitor <vitorlichoti@gmail.com>
2024-03-04 22:59:27 -0400

    adiciona min.css nas docs

--- src/app.js
+++ src/app.js
@@ -5,12 +5,14 @@ const productRouter = require('./routes/product.routes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('../docs/swaggerOptions');
 
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { customCss: CSS_URL }));
 
 app.use(productRouter);
 
